81d8e58e625eb51b2de0bc694e9587ff4d20c1d0
Le107 <157118445+Le107@users.noreply.github.com>
2024-12-21 02:27:10 +0300

    Update install.bat

--- Aimmy-train/install.bat
+++ Aimmy-train/install.bat
@@ -1,9 +1,6 @@
 python -m venv venv
 call venv/scripts/activate
 .\venv\Scripts\python.exe -m pip install --upgrade pip
-pip install ultralytics
-pip uninstall torch torchvision
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu118
-pip install onnx
-pip install tensorboard
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+pip install ultralytics onnx tensorboard
 cmd
